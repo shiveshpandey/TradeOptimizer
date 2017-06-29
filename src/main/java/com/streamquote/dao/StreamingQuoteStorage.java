eb4b00241cf09efeb29db2a04e96922a85ae3dc4
shivesh pandey <shiveshkumarpandey7491@gmail.com>
2017-06-29 22:25:20 +0530

    initial commit

--- src/main/java/com/streamquote/dao/StreamingQuoteStorage.java
+++ src/main/java/com/streamquote/dao/StreamingQuoteStorage.java
@@ -3,6 +3,7 @@ package com.streamquote.dao;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -13,29 +14,30 @@ import com.trade.optimizer.models.Tick;
 
 public interface StreamingQuoteStorage {
 
-	public void initializeJDBCConn();
+    public void initializeJDBCConn();
 
-	public void closeJDBCConn();
+    public void closeJDBCConn();
 
-	public void createDaysStreamingQuoteTable(String date) throws SQLException;
+    public void createDaysStreamingQuoteTable(String date) throws SQLException;
 
-	public void storeData(List<StreamingQuoteModeQuote> quoteList, String tickType);
+    public void storeData(List<StreamingQuoteModeQuote> quoteList, String tickType);
 
-	public ArrayList<Long> getTopPrioritizedTokenList(int i);
+    public ArrayList<Long> getTopPrioritizedTokenList(int i);
 
-	public List<Order> getOrderListToPlace();
+    public List<Order> getOrderListToPlace();
 
-	public void saveInstrumentDetails(List<Instrument> instrumentList, Timestamp time);
+    public void saveInstrumentDetails(List<Instrument> instrumentList, Timestamp time);
 
-	public String[] getInstrumentDetailsOnTokenId(String instrumentToken);
+    public String[] getInstrumentDetailsOnTokenId(String instrumentToken);
 
-	public void saveGeneratedSignals(Map<Long, String> signalList, List<Long> instrumentList);
+    public void saveGeneratedSignals(Map<Long, String> signalList, List<Long> instrumentList);
 
-	public void storeData(ArrayList<Tick> ticks);
+    public void storeData(ArrayList<Tick> ticks);
 
-	public ArrayList<String> getInstrumentTokenIdsFromSymbols(Map<String, Double> stocksSymbolArray);
+    public ArrayList<String> getInstrumentTokenIdsFromSymbols(
+            Map<String, Double> stocksSymbolArray);
 
-	void calculateAndStoreStrategySignalParameters(String instrumentToken, String string);
+    void calculateAndStoreStrategySignalParameters(String instrumentToken, Date timeNow);
 
-	public Map<Long, String> calculateSignalsFromStrategyParams(ArrayList<Long> instrumentList);
+    public Map<Long, String> calculateSignalsFromStrategyParams(ArrayList<Long> instrumentList);
 }
